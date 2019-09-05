bbd539d39c742016a75f8c3aa3434374dd1c68ee
asecoder3 <54946934+asecoder3@users.noreply.github.com>
2019-09-05 18:16:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-console.log(`hello world.`);
+let standard_input = process.stdin;
+standard_input.setEncoding('utf-8');
+standard_input.on('data', function (data) {
+    data = data.slice(0, -2);
+});
