a9a94ca85b4a79909fec6ac2bb41710aaf6388fa
asecoder3 <54946934+asecoder3@users.noreply.github.com>
2019-09-26 18:18:26 +0300

    u

--- public/geolocation.js
+++ public/geolocation.js
@@ -2,11 +2,11 @@ if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition((location) => {
     document.getElementById('longitude').innerHTML = location.coords.longitude;
     document.getElementById('latitude').innerHTML = location.coords.latitude;
-    const map = L.map('map').setView([location.coords.longitude, location.coords.latitude], 5);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    	id: 'mapbox.streets',
-    	accessToken: 'your.mapbox.access.token'
+    const map = L.map('map').setView([location.coords.latitude, location.coords.longitude], 15);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      minZoom: 0,
+      maxZoom: 19,
     }).addTo(map);
   });
 } else {
