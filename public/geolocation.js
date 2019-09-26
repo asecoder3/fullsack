791bed13314902b8d2579d0b0201f75177c091f1
asecoder3 <54946934+asecoder3@users.noreply.github.com>
2019-09-26 18:00:15 +0300

    update

--- public/geolocation.js
+++ public/geolocation.js
@@ -1,11 +1,13 @@
 if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition((location) => {
-    const loc = {
-      longtitude: location.coords.longitude,
-      latitude: location.coords.latitude,
-    };
-    document.getElementById('longtitude').innerHTML = loc.longtitude;
-    document.getElementById('latitude').innerHTML = loc.latitude;
+    document.getElementById('longitude').innerHTML = location.coords.longitude;
+    document.getElementById('latitude').innerHTML = location.coords.latitude;
+    const map = L.map('map').setView([location.coords.longitude, location.coords.latitude], 5);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    	id: 'mapbox.streets',
+    	accessToken: 'your.mapbox.access.token'
+    }).addTo(map);
   });
 } else {
   throw new Error('location not available');
