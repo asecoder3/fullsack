7da3f36f4abcb11d01e3cac484c205ba3c040a85
asecoder3 <54946934+asecoder3@users.noreply.github.com>
2019-10-03 18:17:00 +0300

    responsive this is not

--- public/geolocation.js
+++ public/geolocation.js
@@ -2,13 +2,15 @@ if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition((location) => {
     document.getElementById('longitude').innerHTML = location.coords.longitude;
     document.getElementById('latitude').innerHTML = location.coords.latitude;
-    const map = L.map('map').setView([location.coords.latitude, location.coords.longitude], 15);
+    const crd = [location.coords.latitude, location.coords.longitude];
+    const map = L.map('map').setView(crd, 15);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      attribution: '',
       minZoom: 0,
       maxZoom: 19,
     }).addTo(map);
     $('br').remove();
+    L.marker(crd).addTo(map);
   });
 } else {
   throw new Error('location not available');
