37ed219c4876e943acea545060c485d5858ee601
asecoder3 <54946934+asecoder3@users.noreply.github.com>
2019-10-10 17:30:52 +0300

    u

--- public/app.js
+++ public/app.js
@@ -1,10 +1,1 @@
-const main = {
-  key: '',
-  value: '',
-  create: () => {
-    main.key = (Math.random() * 10000).toString();
-    main.value = (Math.random() * 1000000).toString();
-    document.write(main.key + '<br>' + main.value);
-  }
-};
-document.write(main.key + '<br>' + main.value);
+
--- public/geolocation.js
+++ public/geolocation.js
@@ -1,7 +1,7 @@
 if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition((location) => {
-    document.getElementById('longitude').innerHTML = location.coords.longitude;
-    document.getElementById('latitude').innerHTML = location.coords.latitude;
+    document.getElementById('longitude').innerHTML = location.coords.longitude.toFixed(8);
+    document.getElementById('latitude').innerHTML = location.coords.latitude.toFixed(8);
     const crd = [location.coords.latitude, location.coords.longitude];
     const map = L.map('map').setView(crd, 15);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
