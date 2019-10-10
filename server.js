b5c4f958eba80d6f207dbb9f1fae0499c75536c4
asecoder3 <54946934+asecoder3@users.noreply.github.com>
2019-10-10 17:39:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,4 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.listen(3000, () => console.log(`App listening on port 3000`));
+app.listen(process.env.PORT || 3000, () => console.log(`App listening on port 3000 (?)`));
