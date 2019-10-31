a063a8f263a7a44e0809774dc8b0a489aafa7111
asecoder3 <54946934+asecoder3@users.noreply.github.com>
2019-10-31 18:13:24 +0200

    uu

--- public/app.js
+++ public/app.js
@@ -1,1 +1,35 @@
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition((location) => {
+    document.getElementById('longitude').innerHTML = location.coords.longitude.toFixed(8);
+    document.getElementById('latitude').innerHTML = location.coords.latitude.toFixed(8);
+    const crd = [location.coords.latitude, location.coords.longitude];
+    const map = L.map('map').setView(crd, 15);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '',
+      minZoom: 0,
+      maxZoom: 19,
+    }).addTo(map);
+    $('br').remove();
+    L.marker(crd).addTo(map);
+  });
+} else {
+  throw new Error('location not available');
+}
 
+function openLocationForm() {
+  document.getElementById('placerating').style.display = 'block';
+}
+function cancelLocationSave() {
+  document.getElementById('placerating').style.display = 'none';
+  document.getElementById('placerating-form').reset();
+}
+function savelocation() {
+  document.getElementById('placerating').style.display = 'none';
+  const d = JSON.stringify({
+    place: document.getElementById('place').value,
+    review: document.getElementById('review').value,
+    timestamp: Date.now(),
+  });
+  console.log(d);
+  document.getElementById('placerating-form').reset();
+}
--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 app.listen(process.env.PORT || 3000, () => console.log(`App listening on port 3000 (?)`));
+app.use(express.json({ limit: '1mb', }));
+
+app.post('/api/rating', (req, res) => {
+  console.log('post request');
+  console.log(req.body);
+  req.body ? res.sendStatus(200) : res.sendStatus(400);
+});
